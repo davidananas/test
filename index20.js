cb3292512e480460748556099c16798ac02b31ca
David <116786922+davidananas@users.noreply.github.com>
2024-10-05 12:26:19 +0200

    Add files via upload

---
+++ index20.js
@@ -0,0 +1,12 @@
+let points = 0;
+switch(points) {
+    case "10":
+        console.log("silver age");
+        break;
+    case "0":
+        console.log("bronze age");
+        break;
+    case "100":
+        console.log("gold age");
+        break;
+}
